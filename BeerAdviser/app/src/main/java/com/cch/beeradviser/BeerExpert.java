3d556f6298f290fc6a0579d26ecbee7f138e3c45
CCH0124 <cchong0124@gmail.com>
2019-07-09 09:11:05 +0800

    Basic App

---
+++ BeerAdviser/app/src/main/java/com/cch/beeradviser/BeerExpert.java
@@ -0,0 +1,18 @@
+package com.cch.beeradviser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeerExpert {
+    List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<>();
+        if(color.equals("amber")){
+            brands.add("Jack Amber");
+            brands.add("Red Amber");
+        }else{
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
